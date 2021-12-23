cd1b82c3adc3c7bad8d62ac8a5eca5e4a73a08cb
Arman <life.is2short@outlook.com>
2021-12-23 23:35:28 +0800

    Json的配置从配置文件转到配置类

---
+++ src/main/java/com/student/config/JsonSerializeConfiguration.java
@@ -0,0 +1,37 @@
+package com.student.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * @date 2021/12/23 22:58
+ */
+@Component
+public class JsonSerializeConfiguration {
+
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.createXmlMapper(false)
+                // 任何情况下都序列化该字段，和不写这个注解是一样的效果
+                .serializationInclusion(JsonInclude.Include.ALWAYS)
+                // 全局的时间、时区转化
+                .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+                .timeZone("GMT+8")
+                .locale(Locale.CHINA)
+                // 把 Long 变成 string 在（反）序列化 long类型输出字符串
+                .serializerByType(Long.class, ToStringSerializer.instance)
+                .serializerByType(Long.TYPE, ToStringSerializer.instance)
+                // serialization.write-dates-as-timestamps: false
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
+}
